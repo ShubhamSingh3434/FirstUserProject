4722a5d9f66c26db451b00183fe9446f1e169ffb
ShubhamSingh3434 <singhshubham7855@gmail.com>
2024-02-15 15:00:40 +0530

    first

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp2
+{
+    public class Class1
+    {
+        public string Authenticate(string username, string password)
+        {
+            string msg;
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                msg = "Username and password required";
+            }
+            else
+            {
+                if (username == "admin" || password == "admin@1256")
+                {
+                    msg = "Authentication Pass";
+                }
+                else { msg = "Authenication fail"; }
+            }
+            return msg;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WelcomeApp2;
+Console.WriteLine("Welcome to authentication app");
+Class1 obj = new Class1();
+Console.WriteLine(obj.Authenticate("admin", "admin@1256"));
